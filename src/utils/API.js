b0ab044025c1f28aaff73e39e262ae40f38fc68b
Sherry Zheng <sherryzheng2018@gmail.com>
2021-11-30 12:20:22 -0800

    updating token to connect api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,35 @@
 import axios from "axios";
 
-const URL_PREFIX = "http://localhost:3000"
+const URL_PREFIX = "http://localhost:3001"
 
 
-const API = {
-    getItemById:()=>{
-        return axios.get(`${URL_PREFIX}/api/items/1`)
-    }
+export const API = {
+    login:(loginFormState)=>{
+        return axios.post(`${URL_PREFIX}/api/users/login`, loginFormState)
+    },
+    signup:()=>{
+        return axios.get(`${URL_PREFIX}/api/items`)
+    },
+    getItems:(token, cordinates)=>{
+        if(token){
+            return axios.get(`${URL_PREFIX}/api/query/nearby`,{headers:{
+                "Authorization": `Bearer ${token}`
+            }})
+        } else if(cordinates) {
+            return axios.get(`${URL_PREFIX}/api/items?lon=${cordinates.lon}&lat=${cordinates.lat}`)
+        }
+    },
+    validateToken:(token)=>{
+        return axios.get(`${URL_PREFIX}/api/query/validateToken`, {headers:{
+            "Authorization": `Bearer ${token}`
+        }})
+    },
+    // getItems:()=>{
+    //     return axios.get(`${URL_PREFIX}/api/items`)
+    // },
+    // getItems:()=>{
+    //     return axios.get(`${URL_PREFIX}/api/items`)
+    // },
 }
 
 export default API;
