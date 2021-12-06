93aaa674e645ca99699633339223ce024f850e7d
Sherry Zheng <sherryzheng2018@gmail.com>
2021-12-06 14:34:07 -0800

    miner change

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,104 +1,104 @@
 import {
-    FormControlLabel,
-    FormGroup,
-    Switch,
-    TextField,
-    Button,
-    Typography,
-    Grid
-  } from "@material-ui/core";
-  import { useAlert } from "react-alert";
-  import styles from "./styles.module.css";
-  import { Link, useNavigate } from "react-router-dom";
-  import { useEffect, useState } from "react";
-  import { getLocation } from "../../utils/location";
-  import { IKContext, IKImage, IKUpload } from 'imagekitio-react';
-  import { API } from "../../utils/API"
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  TextField,
+  Button,
+  Typography,
+  Grid
+} from "@material-ui/core";
+import { useAlert } from "react-alert";
+import styles from "./styles.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getLocation } from "../../utils/location";
+import { IKContext, IKImage, IKUpload } from 'imagekitio-react';
+import { API } from "../../utils/API"
 
 const publicKey = 'public_t+4VajkBmNbytb2Sa80EQD4geXo=';
 const urlEndpoint = 'https://ik.imagekit.io/beebyapp';
 const authenticationEndpoint = API.authUrl
-  const SignUp = () => {
-    const navigate = useNavigate();
-    
-    // eslint-disable-next-line
-  
-    const alert = useAlert();
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmedPassword, setConfirmedPassword] = useState("");
-    const [position, setPosition] = useState([]);
-    const [allowLocation, setAllowLocation] = useState(false);
-    const [bio, setBio] = useState("")
-    const [userImg, setImgUser] = useState("")
-    // const [allowNotication, setAllowNotication] = useState(true);
-  
-    const onError = err => {
-      console.log("Error", err);
-    };
-    const onSuccess = res => {
-      console.log("Success", res)
-      setImgUser(...userImg, res.url)
-    };
+const SignUp = () => {
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      if (allowLocation) {
-        getLocation()
-          .then((pos) => {
-            setPosition(pos);
-          })
-          .catch((err) => {
-            alert.error(err);
-          });
-      } else {
-        setPosition([]);
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [allowLocation]);
-  
-    const handleSubmit = async () => {
-      console.log(userImg)
-      if (!firstname || !lastname || !email || !password) {
-        alert.error("Please enter all fields!");
-        return;
-      }
-      if (password.length < 8) {
-        alert.error("Password length must more than eight!");
-        return;
-      }
-      if (password !== confirmedPassword) {
-        alert.error("Password is not same as confirm password!!");
-        return;
-      }
-      try {
-        await API.signup(
-          firstname,
-          lastname,
-          email,
-          password,
-          position[0],
-          position[1],
-          bio,
-          userImg
-        );
-        alert.success("Success to sign up!!");
-        setTimeout(() => {
-          navigate("/signin");
-        }, 1000);
-      } catch (err) {
-        // const errors = err.response.data.err.errors;
-        console.log(err)
-        // if (errors[0] && errors[0].message) {
-        //   alert.error(errors[0].message);
-        // }
-        //alert.error(err.response.data);
-      }
-    };
-  
-    return (
-<Grid container className={styles.wrapper}>
+  // eslint-disable-next-line
+
+  const alert = useAlert();
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [position, setPosition] = useState([]);
+  const [allowLocation, setAllowLocation] = useState(false);
+  const [bio, setBio] = useState("")
+  const [userImg, setImgUser] = useState("")
+  // const [allowNotication, setAllowNotication] = useState(true);
+
+  const onError = err => {
+    console.log("Error", err);
+  };
+  const onSuccess = res => {
+    console.log("Success", res)
+    setImgUser(...userImg, res.url)
+  };
+
+  useEffect(() => {
+    if (allowLocation) {
+      getLocation()
+        .then((pos) => {
+          setPosition(pos);
+        })
+        .catch((err) => {
+          alert.error(err);
+        });
+    } else {
+      setPosition([]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [allowLocation]);
+
+  const handleSubmit = async () => {
+    console.log(userImg)
+    if (!firstname || !lastname || !email || !password) {
+      alert.error("Please enter all fields!");
+      return;
+    }
+    if (password.length < 8) {
+      alert.error("Password length must more than eight!");
+      return;
+    }
+    if (password !== confirmedPassword) {
+      alert.error("Password is not same as confirm password!!");
+      return;
+    }
+    try {
+      await API.signup(
+        firstname,
+        lastname,
+        email,
+        password,
+        position[0],
+        position[1],
+        bio,
+        userImg
+      );
+      alert.success("Success to sign up!!");
+      setTimeout(() => {
+        navigate("/signin");
+      }, 1000);
+    } catch (err) {
+      // const errors = err.response.data.err.errors;
+      console.log(err)
+      // if (errors[0] && errors[0].message) {
+      //   alert.error(errors[0].message);
+      // }
+      //alert.error(err.response.data);
+    }
+  };
+
+  return (
+    <Grid container className={styles.wrapper}>
       <Grid item marginTop="40px" xs={12} lg={12}>
         <Typography
           textAlign="center"
@@ -115,146 +115,145 @@ const authenticationEndpoint = API.authUrl
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
-          <TextField
-            fullWidth
-            label="First name"
-            value={firstname}
-            onChange={(e) => setFirstname(e.target.value)}
-            variant="outlined"
-          />
-         </Grid>
+        <TextField
+          fullWidth
+          label="First name"
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+          variant="outlined"
+        />
+      </Grid>
       <Grid
         item
         xs={10}
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
-          <TextField
-            fullWidth
-            label="Last name"
-            value={lastname}
-            onChange={(e) => setLastname(e.target.value)}
-            variant="outlined"
-          />
-        </Grid>
+        <TextField
+          fullWidth
+          label="Last name"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+          variant="outlined"
+        />
+      </Grid>
       <Grid
         item
         xs={10}
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
-          <TextField
-            fullWidth
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            variant="outlined"
-            />
-            </Grid>
-            <Grid
-              item
-              xs={10}
-              lg={4}
-              style={{ display: "flex", justifyContent: "center" }}
-            >
-          <TextField
-            fullWidth
-            label="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            variant="outlined"
-          />
-         </Grid>
+        <TextField
+          fullWidth
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          variant="outlined"
+        />
+      </Grid>
       <Grid
         item
         xs={10}
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
-          <TextField
-            fullWidth
-            value={confirmedPassword}
-            onChange={(e) => setConfirmedPassword(e.target.value)}
-            label="Confirm Password"
-            type="password"
-            variant="outlined"
-          />
-        </Grid>
+        <TextField
+          fullWidth
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          variant="outlined"
+        />
+      </Grid>
       <Grid
         item
         xs={10}
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
-          <TextField
-            fullWidth
-            label="Bio"
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            variant="outlined"
-            multiline
-            rows={2}
-            rowsMax={4}
-          />
-        </Grid>
+        <TextField
+          fullWidth
+          value={confirmedPassword}
+          onChange={(e) => setConfirmedPassword(e.target.value)}
+          label="Confirm Password"
+          type="password"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid
+        item
+        xs={10}
+        lg={4}
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <TextField
+          fullWidth
+          label="Bio"
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+          variant="outlined"
+          multiline
+          rows={2}
+          rowsMax={4}
+        />
+      </Grid>
 
-        <Grid
-          item
-          xs={10}
-          lg={4}
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch
-                  defaultChecked={false}
-                  value={allowLocation} // add description to show why they need to share location
-                  onChange={(e) => setAllowLocation(e.target.checked)}
-                />
-              }
-              label="Share Location"
-            />
-            <IKContext 
-              publicKey={publicKey} 
-              urlEndpoint={urlEndpoint} 
-              authenticationEndpoint={authenticationEndpoint} >
-              <IKUpload
-                fileName="user.jpg"
-                isPrivateFile={false}
-                useUniqueFileName={true}
-                folder={"/userImg"}
-                onError={onError}
-                onSuccess={onSuccess}
+      <Grid
+        item
+        xs={10}
+        lg={4}
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                defaultChecked={false}
+                value={allowLocation} // add description to show why they need to share location
+                onChange={(e) => setAllowLocation(e.target.checked)}
               />
-            </IKContext>
-          </FormGroup>
-          </Grid>
-        
-          <Grid
+            }
+            label="Share Location"
+          />
+          <IKContext
+            publicKey={publicKey}
+            urlEndpoint={urlEndpoint}
+            authenticationEndpoint={authenticationEndpoint} >
+            <IKUpload
+              fileName="user.jpg"
+              isPrivateFile={false}
+              useUniqueFileName={true}
+              folder={"/userImg"}
+              onError={onError}
+              onSuccess={onSuccess}
+            />
+          </IKContext>
+        </FormGroup>
+      </Grid>
+
+      <Grid
         item
         xs={10}
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >          <Typography variant="body2">Already has Account?</Typography>
-          <Link to="/signin">Login</Link>
-        </Grid>
+        <Link to="/signin">Login</Link>
+      </Grid>
 
-        <Grid
+      <Grid
         item
         xs={10}
         lg={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
-          <Button onClick={handleSubmit} variant="contained">
-            Submit
-          </Button>
-        </Grid>
-        </Grid>
-    );
-  };
-  
-  export default SignUp;
-  
+        <Button onClick={handleSubmit} variant="contained">
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SignUp;
